073540f1275d4f1ec681b3233acee53935c2b3da
AlexandrMaltsevYDX <AlexandrMaltsve@yandex.ru>
2024-02-11 08:08:10 +0100

    qwer

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path"; // ! yarn add @types/node
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
   resolve: {
     alias: {
       "~shared": path.resolve(__dirname, "./src/06_shared"),
